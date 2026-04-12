b57422bc6fcb47f4a7a4ec7c596623ac13f248d4
Nikolay <WyomingCPA@yandex.ru>
2026-04-12 08:19:30 +0300

    update api telegram bot

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -14,11 +14,11 @@ class ApiController extends Controller
     public function storeSmoke(Request $request, CoinService $coinService)
     {
         $user = User::first(); // если пользователь один
-
         $coinService->registerSmoke($user);
+        $lastSmoke = Smoke::latest('smoked_at')->first();
 
         return response()->json([
-            
+            'last_smoke' => $lastSmoke->created_at,
             'status' => true,
         ], 200);
     }
